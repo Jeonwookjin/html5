d909a50ede69cf51c75ac7cbbe2edac9e86b6df2
Jeonwookjin <c10340@lx.or.kr>
2025-08-07 16:47:37 +0900

    유머튜브

--- humortube/src/App.jsx
+++ humortube/src/App.jsx
@@ -5,10 +5,13 @@ function App() {
     <>
     <div className="center_div">
     <h1>유튜브 영상 모음</h1>
+
 <h3>옵티머스 3세대</h3>
    <iframe width="560" height="315" src="https://www.youtube.com/embed/keCpS4wrSxw?si=tmLI27ObAakmIvxq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
    <h3>뉴럴링크</h3>
    <iframe width="560" height="315" src="https://www.youtube.com/embed/MtYAFnLahbU?si=H7bPj5eByHJv5QAU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+   
    <h3>로보택시</h3>
    <iframe width="560" height="315" src="https://www.youtube.com/embed/CZq4jkVIgjM?si=AU70Lpbf7LTPu4mK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
    </div>
